bfc914a3d747dcf4c9f02b66bafe728fd1073bfa
Steve Winton <stevewinton@gmail.com>
2012-12-19 15:33:05 +0000

    Syntax error in project/Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-        "postgresql" % "postgresql" % "8.4-702.jdbc4"
+        "postgresql" % "postgresql" % "8.4-702.jdbc4",
         "com.joestelmach" % "natty" % "0.7"
     )
 
